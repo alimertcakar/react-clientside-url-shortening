9ce38643fba2d614fadac23f968edfcaec79f974
Ali Mert <mail@alimert.net>
2020-06-02 14:11:20 +0300

    unimportant

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
 import { StylesProvider } from '@material-ui/core/styles';
 import Routes from "./components/Router/router.component";
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+  margin:0
+  }
+`
+
 function App() {
   return (
     <StylesProvider injectFirst>
-      <Container>
-        <Routes />
-      </Container>
+      <Routes />
+      <GlobalStyle />
     </StylesProvider>
   );
 }
--- src/components/Home/home.component.jsx
+++ src/components/Home/home.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AppBar from "../Navigation/AppBar.component";
+import { Container } from "@material-ui/core";
+
 export default () => {
   return (
     <div>
-      Home
       <AppBar />
+      <Container></Container>
     </div>
   );
 };
